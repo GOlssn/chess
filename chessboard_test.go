c563868e519478be73df0f6d471ac40a1fa9f411
gustav <gustav.olsson@klarna.com>
2019-02-05 15:32:12 +0100

    Working valid pawn move generation for White

--- chessboard_test.go
+++ chessboard_test.go
@@ -12,7 +12,8 @@ func TestValidPawnMoves(t *testing.T) {
 		want  Bitboard
 	}{
 		{Chessboard{WhitePawns: 0x8000}, White, Bitboard(0x80800000)},
-		{Chessboard{WhitePawns: 0x800000, BlackPawns: 0x40000000, BlackPieces: 0x800000 & 0x40000000}, White, Bitboard(0xc0000000)},
+		{Chessboard{WhitePawns: 0x800000, BlackPieces: 0x40000000}, White, Bitboard(0xc0000000)},
+		{Chessboard{WhitePawns: 0x20000400, BlackPieces: 0x1000000000}, White, Bitboard(0x3004040000)},
 	}
 
 	for _, tc := range testCases {
